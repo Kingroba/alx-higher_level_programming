9f4d163d7d65834908af29bba9f2381dc9331c8f
Robel Bekele <robba836@gmail.com>
2023-06-12 12:33:50 -0700

    task 13 more update

--- 0x03-python-data_structures/linked_lists.c
+++ 0x03-python-data_structures/linked_lists.c
@@ -1,82 +1,74 @@
-#ifndef LISTS_H
-#define LISTS_H
-
 #include <stdio.h>
 #include <stdlib.h>
+#include "lists.h"
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
  */
-typedef struct listint_s
+size_t print_listint(const listint_t *h)
 {
-  int n;
-  struct listint_s *next;
-};
+    const listint_t *current;
+    unsigned int n; /* number of nodes */
+
+    current = h;
+    n = 0;
+    while (current != NULL)
+    {
+        printf("%i\n", current->n);
+        current = current->next;
+        n++;
+    }
+
+    return (n);
+}
 
 /**
- * size_t print_listint(const listint_t *h);
- *
- * Description: prints the values of all the nodes in a linked list
- *
- * Parameters:
- *   h - The head of the linked list
- *
- * Returns:
- *   The number of nodes in the linked list
- *
- * Comments:
- *   This function iterates over all the nodes in the linked list and prints their values.
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * @n: integer to be included in new node
+ * Return: address of the new element or NULL if it fails
  */
-size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+    listint_t *new;
+    listint_t *current;
+
+    current = *head;
+
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->n = n;
+    new->next = NULL;
+
+    if (*head == NULL)
+        *head = new;
+    else
+    {
+        while (current->next != NULL)
+            current = current->next;
+        current->next = new;
+    }
+
+    return (new);
+}
 
 /**
- * listint_t *add_nodeint_end(listint_t **head, const int n);
- *
- * Description: adds a new node to the end of a linked list
- *
- * Parameters:
- *   head - A pointer to the head of the linked list
- *   n - The value of the new node
- *
- * Returns:
- *   A pointer to the new node
- *
- * Comments:
- *   This function allocates a new node and initializes its values. The new node is then added to the end of the linked list.
+ * free_listint - frees a listint_t list
+ * @head: pointer to list to be freed
+ * Return: void
  */
-listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head)
+{
+    listint_t *current;
 
-/**
- * void free_listint(listint_t *head);
- *
- * Description: frees all the nodes in a linked list
- *
- * Parameters:
- *   head - The head of the linked list
- *
- * Comments:
- *   This function iterates over all the nodes in the linked list and frees them.
- */
-void free_listint(listint_t *head);
-
-/**
- * int is_palindrome(listint_t **head);
- *
- * Description: checks if a linked list is a palindrome
- *
- * Parameters:
- *   head - A pointer to the head of the linked list
- *
- * Returns:
- *   1 if the linked list is a palindrome, 0 otherwise
- *
- * Comments:
- *   This function checks if a linked list is a palindrome by comparing the values of the nodes in the list from the beginning to the end.
- */
-int is_palindrome(listint_t **head);
-
-#endif /* LISTS_H */
+    while (head != NULL)
+    {
+        current = head;
+        head = head->next;
+        free(current);
+    }
+}
