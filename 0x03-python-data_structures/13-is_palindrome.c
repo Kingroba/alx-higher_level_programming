ce13ac2dad299b33a9f2500f5c7eb7f64a7914be
Robel Bekele <robba836@gmail.com>
2023-06-12 12:25:47 -0700

    2nd update for task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,96 +1,70 @@
-#include "lists.h"
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "lists.h"
 
-// Structure for a node in the linked list
-typedef struct ListNode {
-    int val;
-    struct ListNode* next;
-} ListNode;
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
 
-// Function to check if a singly linked list is a palindrome
-int is_palindrome(ListNode** head) {
-    // Empty list is considered a palindrome
-    if (*head == NULL) {
-        return 1;
-    }
-    
-    // Find the middle node of the list
-    ListNode* slow = *head;
-    ListNode* fast = *head;
-    
-    while (fast != NULL && fast->next != NULL) {
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-    
-    // Reverse the second half of the list
-    ListNode* prev = NULL;
-    ListNode* curr = slow;
-    
-    while (curr != NULL) {
-        ListNode* next = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = next;
-    }
-    
-    // Compare the first half and the reversed second half of the list
-    ListNode* firstHalf = *head;
-    ListNode* secondHalf = prev;
-    
-    while (secondHalf != NULL) {
-        if (firstHalf->val != secondHalf->val) {
-            return 0;  // Not a palindrome
-        }
-        firstHalf = firstHalf->next;
-        secondHalf = secondHalf->next;
-    }
-    
-    return 1;  // It is a palindrome
+    head = NULL;
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 17);
+    add_nodeint_end(&head, 972);
+    add_nodeint_end(&head, 50);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 50);
+    add_nodeint_end(&head, 972);
+    add_nodeint_end(&head, 17);
+    add_nodeint_end(&head, 1);
+    print_listint(head);
+
+    if (is_palindrome(&head) == 1)
+        printf("Linked list is a palindrome\n");
+    else
+        printf("Linked list is not a palindrome\n");
+
+    free_listint(head);
+
+    return (0);
 }
 
-// Helper function to create a new node with a given value
-ListNode* createNode(int value) {
-    ListNode* newNode = (ListNode*)malloc(sizeof(ListNode));
-    newNode->val = value;
-    newNode->next = NULL;
-    return newNode;
-}
 
-// Helper function to insert a new node at the beginning of the list
-void insertAtBeginning(ListNode** head, int value) {
-    ListNode* newNode = createNode(value);
-    newNode->next = *head;
-    *head = newNode;
-}
 
-// Test the is_palindrome function
-int main() {
-    // Create a sample linked list: 1 -> 2 -> 3 -> 2 -> 1
-    ListNode* head = NULL;
-    insertAtBeginning(&head, 1);
-    insertAtBeginning(&head, 2);
-    insertAtBeginning(&head, 3);
-    insertAtBeginning(&head, 2);
-    insertAtBeginning(&head, 1);
-    
-    // Check if the linked list is a palindrome
-    int result = is_palindrome(&head);
-    
-    if (result == 1) {
-        printf("The linked list is a palindrome.\n");
-    } else {
-        printf("The linked list is not a palindrome.\n");
-    }
-    
-    // Free the memory
-    ListNode* current = head;
-    while (current != NULL) {
-        ListNode* temp = current;
-        current = current->next;
-        free(temp);
-    }
-    
-    return 0;
-}
+
+
+2-main.py
+
+#!/usr/bin/python3
+replace_in_list = __import__('2-replace_in_list').replace_in_list
+
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 9
+new_list = replace_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)
+
+
+
+
+
+
+2-replace_in_list.py
+
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list
+
