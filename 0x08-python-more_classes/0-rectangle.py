5633252b0bf556d470a4e2a5c15d22e2b81d53d9
Robel Bekele <robba836@gmail.com>
2023-07-03 06:27:54 -0700

    Task 0 2nd update

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
+"""
+This script defines a Rectangle class.
+"""
+
+
 class Rectangle:
-    """Represents a rectangle."""
+    """
+    Represents a rectangle.
+    """
     pass
+
