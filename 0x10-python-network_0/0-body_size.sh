3e0ca75a973bbeb32279ad499b6c316af40f7921
Robel Bekele <robba836@gmail.com>
2023-10-01 14:56:07 -0700

    task 0 done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-# Check if URL argument is provided
-if [ $# -eq 0 ]; then
-    echo "Error: No URL provided."
-    exit 1
-fi
-
-# Sends a request to the provided URL and displays the size of the response body in bytes
+#takes in a URL, sends a request to that URL,and displays the size of the body of the response
 curl -s "$1" | wc -c
-
